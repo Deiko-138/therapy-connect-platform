19395b5a16d99a9874555f43bb1d15d0bc551c3b
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-06-09 18:13:37 -0500

    chore: initial commit.

---
+++ src/main/java/com/open/therapyconnect/platform/TherapyConnectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.open.therapyconnect.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapyConnectPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapyConnectPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open/therapyconnect/platform/TherapyConnectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.therapyconnect.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapyConnectPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
