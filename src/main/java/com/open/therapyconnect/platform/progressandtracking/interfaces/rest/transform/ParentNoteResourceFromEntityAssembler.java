b91af21fc4f848ba39c7c4399366c0f47a58fd06
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-06-18 22:18:09 -0500

    feat(ProgressTracking): add ParentNotesController and ParentNoteResourceFromEntityAssembler for managing parent notes

---
+++ src/main/java/com/open/therapyconnect/platform/progressandtracking/interfaces/rest/transform/ParentNoteResourceFromEntityAssembler.java
@@ -0,0 +1,28 @@
+package com.open.therapyconnect.platform.progressandtracking.interfaces.rest.transform;
+
+import com.open.therapyconnect.platform.progressandtracking.domain.model.aggregates.ParentNote;
+import com.open.therapyconnect.platform.progressandtracking.interfaces.rest.resources.ParentNoteResource;
+
+/**
+ * Assembler to convert a ParentNote entity to a ParentNoteResource.
+ */
+public class ParentNoteResourceFromEntityAssembler {
+
+    /**
+     * Converts a ParentNote entity to a ParentNoteResource.
+     *
+     * @param entity The {@link ParentNote} entity to convert.
+     * @return The {@link ParentNoteResource} resource that results from the conversion.
+     */
+    public static ParentNoteResource toResourceFromEntity(ParentNote entity) {
+        return new ParentNoteResource(
+                entity.getId(),
+                entity.getNoteDate().value(),
+                entity.getContent().value(),
+                entity.getConditionType(),
+                entity.getConditionDescription().value(),
+                entity.getAuthorProfileId().id(),
+                entity.getNextSteps().value()
+        );
+    }
+}
