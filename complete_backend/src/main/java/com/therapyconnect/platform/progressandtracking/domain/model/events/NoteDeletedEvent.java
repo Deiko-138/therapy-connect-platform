43ce3d388476c031aaa278955716d712e36e6610
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-06-18 22:20:27 -0500

    feat(ProgressTracking): update pom.xml with project name and description; modify RolePersistenceEntity name column length

--- complete_backend/src/main/java/com/therapyconnect/platform/progressandtracking/domain/model/events/NoteDeletedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.therapyconnect.platform.progressandtracking.domain.model.events;
-
-/**
- * Domain event raised when a note is deleted.
- *
- * @param noteId the id of the deleted note
- */
-public record NoteDeletedEvent(Long noteId) {
-}
--- complete_backend/src/main/java/com/therapyconnect/platform/progressandtracking/domain/model/valueobjects/TeacherNoteType.java
+++
@@ -1,9 +0,0 @@
-package com.therapyconnect.platform.progressandtracking.domain.model.valueobjects;
-
-/**
- * Enum value object representing the plan type of a teacher note.
- */
-public enum TeacherNoteType {
-    PERSONAL,
-    INSTITUTIONAL
-}
