92bd84d7a07fd7f36a821e75b141cf2f85d6149b
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-06-18 22:15:44 -0500

    feat(ProgressTracking): add TeacherNoteType enum for teacher note plan types

---
+++ complete_backend/src/main/java/com/therapyconnect/platform/progressandtracking/domain/model/valueobjects/TeacherNoteType.java
@@ -0,0 +1,9 @@
+package com.therapyconnect.platform.progressandtracking.domain.model.valueobjects;
+
+/**
+ * Enum value object representing the plan type of a teacher note.
+ */
+public enum TeacherNoteType {
+    PERSONAL,
+    INSTITUTIONAL
+}
