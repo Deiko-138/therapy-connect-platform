8fc0327ccb19537a4548a09219eada469d06e89c
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-06-18 22:15:15 -0500

    feat(Application): add main application class and initial test setup

---
+++ complete_backend/src/test/java/com/therapyconnect/platform/TherapyConnectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therapyconnect.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TherapyConnectPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
